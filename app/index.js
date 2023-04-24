e74d253875bb54daf410960bb35056c367f4d229
kooya3 <elyeestatua@gmail.com>
2023-04-24 12:44:31 +0300

    Defined a router hook

--- app/index.js
+++ app/index.js
@@ -3,8 +3,13 @@ import { View, ScrollView, SafeAreaView } from 'react-native';
 import { Stack, useRouter } from 'expo-router';
 
 import { COLORS, icons, images, SIZES } from '../constants';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components'
 
 const Home = () => {
+
+    const router = useRouter();
+
+
     <View>
         <Text>Home</Text>
     </View>
