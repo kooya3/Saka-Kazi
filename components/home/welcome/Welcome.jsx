6444c29c3a30dd6b11f12cbe44729323031f7794
kooya3 <elyeestatua@gmail.com>
2023-04-25 12:42:21 +0300

    created a import route in welcome page

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
-import { View, Text } from 'react-native'
 
-import styles from './welcome.style'
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+import { useRouter } from "expo-router";
+
 
 const Welcome = () => {
   return (
