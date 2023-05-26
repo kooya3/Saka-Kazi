a9e8aa000eaee6d131226bcbf30c0ba79b520440
kooya3 <elyeestatua@gmail.com>
2023-05-26 16:20:59 +0300

    started on the backend

---
+++ hook/useFetch.js
@@ -0,0 +1,7 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useFetch = () => {
+    const [data, setData] = useState([]);
+    const [isLoading, setISLoading] = useState(false);
+}
