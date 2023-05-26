651726d99d5ca11239b9944db93b61dc3e9ad11f
kooya3 <elyeestatua@gmail.com>
2023-05-26 14:15:32 +0300

    created an important edit

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -21,7 +21,7 @@ const Welcome = ({ searchTerm, setSearchTerm, handleClick }) => {
   return (
     <View>
       <View style={styles.container}>
-        <Text style={styles.userName}>Hello Adrian</Text>
+        <Text style={styles.userName}>Hello Elyees</Text>
         <Text style={styles.welcomeMessage}>Find your perfect job</Text>
       </View>
 
@@ -31,7 +31,7 @@ const Welcome = ({ searchTerm, setSearchTerm, handleClick }) => {
             style={styles.searchInput}
             value={searchTerm}
             onChangeText={(text) => setSearchTerm(text)}
-            placeholder='What are you looking for?'
+            placeholder='What you looking for?'
           />
         </View>
 
