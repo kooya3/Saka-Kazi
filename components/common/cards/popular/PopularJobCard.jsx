17d75ec91b25b82d1f66779d6726948ef878c99c
kooya3 <elyeestatua@gmail.com>
2023-05-27 07:29:50 +0300

    specifying checkImageURL route

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -2,6 +2,8 @@ import { View, Text, TouchableOpacity, Image } from 'react-native'
 
 import styles from './popularjobcard.style'
 
+import { checkImageURL } from '../../../../utils';
+
 const PopularJobCard = () => {
   return (
     <View>
